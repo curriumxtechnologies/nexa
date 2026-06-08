3376a344fd2077bb391d808d911b88a6825641bc
flameztech01 <flameztech01@gmail.com>
2026-06-08 21:06:50 +0100

    new

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(cors({
     origin: [
         'http://localhost:5173',
         'https://nexa.curriumx.online',
-        'http://localhost',
+        'https://localhost',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH','OPTIONS'],
